a4ee6f23f54368985c462b800c6e42faf0a6eb98
Michael Katos <katoskreations@gmail.com>
2019-05-21 12:22:54 -0400

    Set up Initial page routes=

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const About = () => (
+  <section>
+    <h1>About Next JS</h1>
+    <Link href="/">
+      <a href="/" title="NextJS Tutorial">
+        Home
+      </a>
+    </Link>
+  </section>
+);
+
+export default About;
